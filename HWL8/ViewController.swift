ca39d31545ea9c2d31974e166e3481768cd00fab
DanilEVG <89922067067@mail.ru>
2024-05-02 13:58:14 +0500

    Initial Commit

---
+++ HWL8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWL8
+//
+//  Created by Даниил Евгеньевич on 02.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
