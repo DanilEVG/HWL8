7afdd887976963a32c0fd7be20cb0653bccffb13
DanilEVG <89922067067@mail.ru>
2024-05-15 21:20:08 +0500

    hwl8

--- HWL8/ViewController.swift
+++ HWL8/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func submitTapped(_ sender: UIButton) {
+        firstNameTextField.text = ""
+        lastNameTextField.text = ""
+    }
+    
 }
 
